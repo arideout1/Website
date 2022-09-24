96bf360ff4d3703686747ffdef333fd65562b4bd
arideout1 <adamrideout@outlook.com>
2022-09-23 17:05:47 -0700

    Added divider class

--- src/components/MainPages/About.js
+++ src/components/MainPages/About.js
@@ -1,21 +1,27 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './About.module.css';
+import Divider from '../Utils/Divider';
+import { DividerChoices } from '../Utils/Divider';
 
 function About() {
-    const [isNavOpen, setIsNavOpen] = useState(false)
-
-    useEffect(() => {
-        setIsNavOpen(true);
-    });
 
     return (
-        <div className={`${styles.wrapper}  ${isNavOpen ? styles.isNavOpen : ""}`}>
-            <div className={`${styles.nav}}`}>
-                <button onClick={() => setIsNavOpen(v => !v)}>
-                    This is a button
-                </button>
-                <div>
-                    THis is test text
+        <div className={styles.sections}>
+            <div className={styles.section}>
+                <div className={styles.title}>
+                    My Mission
+                </div>
+            </div>
+            <Divider direction={DividerChoices.Vertical} padding={true} />
+            <div className={styles.section}>
+                <div className={styles.title}>
+                    My Background
+                </div>
+            </div>
+            <Divider direction={DividerChoices.Vertical} padding={true} />
+            <div className={styles.section}>
+                <div className={styles.title}>
+                    My Skills
                 </div>
             </div>
         </div>
