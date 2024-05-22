01c583b2d9b2481a1f69d8e51fb0549799cec5a7
Adam Rideout <adamrideout@outlook.com>
2024-05-22 00:07:12 -0700

    Cleanup

--- src/components/Challenges/DiceFace.js
+++ src/components/Challenges/DiceFace.js
@@ -1,15 +1,8 @@
-import { useEffect, useState } from "react";
 import styles from "./DiceFace.module.css";
 
 export const DiceFace = (props) => {
-  const [spin, setSpin] = useState(false);
-
-  useEffect(() => {
-    setSpin(!spin);
-  }, [props.triggerRoll]);
-
   return (
-    <div className={spin ? styles.dice : styles.dice2}>
+    <div className={props.triggerRoll ? styles.dice : styles.dice2}>
       {props.letter === "Q" ? "Qu" : props.letter}
     </div>
   );
